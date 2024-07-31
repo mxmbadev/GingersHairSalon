448a4611744e98c574db6d7840e49185e5bb9d92
Brandon Espinoza <espinozab100@gmail.com>
2024-07-31 02:00:22 -0500

    updating ssg.php to copy other assets

--- config/statamic/ssg.php
+++ config/statamic/ssg.php
@@ -40,6 +40,10 @@ return [
 
     'copy' => [
         public_path('build') => 'build',
+        public_path('css') => 'css',
+        public_path('js') => 'js',
+        public_path('assets') => 'assets',
+        public_path('favicon.ico') => 'favicon.ico',
     ],
 
     'symlinks' => [
