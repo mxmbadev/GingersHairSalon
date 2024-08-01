6afe5a652a076914691425ae9bba7fb2bf108ef4
Brandon Espinoza <espinozab100@gmail.com>
2024-07-31 21:04:16 -0500

    updated vite config js

--- vite.config.js
+++ vite.config.js
@@ -10,5 +10,6 @@ export default defineConfig({
             ],
             refresh: true,
         }),
-    ]
+    ],
+    base: process.env.NODE_ENV === 'production' ? 'https://salongingers.netlify.app/' : '/',
 });
