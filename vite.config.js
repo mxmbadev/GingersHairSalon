f4929c342d6c080acb23dbf0de2fee5f193e8c76
Brandon Espinoza <espinozab100@gmail.com>
2024-08-05 21:37:21 -0500

    configured vite config file for scss files

--- vite.config.js
+++ vite.config.js
@@ -5,11 +5,24 @@ export default defineConfig({
     plugins: [
         laravel({
             input: [
-                'resources/scss/main.scss', // Entry point for your SCSS
+                'resources/scss/main.scss',
                 'resources/js/site.js'
             ],
             refresh: true,
         }),
     ],
-    base: process.env.NODE_ENV === 'production' ? '/' : '/',
+    build: {
+        rollupOptions: {
+            output: {
+                entryFileNames: 'js/[name].js',
+                chunkFileNames: 'js/[name].js',
+                assetFileNames: ({ name }) => {
+                    if (name.endsWith('.css')) {
+                        return 'css/site.css';
+                    }
+                    return 'assets/[name].[ext]';
+                },
+            },
+        },
+    },
 });
